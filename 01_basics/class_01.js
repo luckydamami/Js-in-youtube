fef51862b5894db89e9a3c9558b03db32b9c57b7
luckydamami <157327234+luckydamami@users.noreply.github.com>
2024-04-29 09:42:03 +0000

    Class Datatypes

---
+++ 01_basics/class_01.js
@@ -0,0 +1,31 @@
+/*const account_Id = 13382;
+let account_Email = "luckydamamii2410@gmail.com";
+account_City = "jaipur";
+// account_id = 34; 
+account_Email = "aniketdamamii2420@gmail.com";
+account_Password = "3763";
+account_City = "Delhi";
+
+/*
+    Prefer not to use var because of issue in block scope and functional scope   
+*
+console.table([account_Email, account_Password, account_City]);*/
+"use strict";
+let fullname = "Aniket";
+let age = 18;
+let islogdIn = 1;
+
+// Premitive Datatypes in javascript
+//Number
+//BigInt
+//String
+//Boolean => true/false
+//null
+//undefined
+//Symbol => use for uniqueness
+
+//Non premitive Datatypes
+//Object
+
+let Booleanislogged = Boolean(islogdIn);
+console.log(Booleanislogged);
