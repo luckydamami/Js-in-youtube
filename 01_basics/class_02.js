0aed396dd603a96ec057a904bc1981ee187e4d86
luckydamami <157327234+luckydamami@users.noreply.github.com>
2024-04-30 08:07:33 +0000

    Data type summary

---
+++ 01_basics/class_02.js
@@ -0,0 +1,22 @@
+//  #premitive  -- call by value
+//  7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+//  Non premitive(Reference type) -- call by reference 
+
+//  types : Array, Objects, Functions
+//  Master of javascript : Objects, Browser's web events.
+
+const run = 38;
+const Name = "lucky";
+const isRight = true;
+let usermail;
+const id = Symbol('123');
+const anotherId = Symbol('123');
+
+//console.log(id === anotherId);
+const myarray = ["one","two","three","four"];
+let myObj = {
+    name : "aniket",
+    age : 22,
+}
+console.log(typeof myObj);
